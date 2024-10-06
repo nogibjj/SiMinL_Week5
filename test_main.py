9c1bb05ddd03634628e890f52d093701b0fae0c0
loosimin16 <sl913@duke.edu>
2024-10-06 00:10:00 -0400

    2

--- test_main.py
+++ test_main.py
@@ -30,6 +30,25 @@ def test_transform_load():
     assert "Transforming data..." in result.stdout
 
 
+def test_create_record():
+    """tests create_record"""
+    result = subprocess.run(
+        [
+            "python",
+            "main.py",
+            "create_record",
+            "Computer Science",
+            "STEM",
+            "1500",
+            "1200",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
 def test_update_record():
     """tests update_record"""
     result = subprocess.run(
@@ -57,25 +76,6 @@ def test_delete_record():
     """tests delete_record"""
     result = subprocess.run(
         ["python", "main.py", "delete_record", "1"],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    assert result.returncode == 0
-
-
-def test_create_record():
-    """tests create_record"""
-    result = subprocess.run(
-        [
-            "python",
-            "main.py",
-            "create_record",
-            "Computer Science",
-            "STEM",
-            "1500",
-            "1200",
-        ],
         capture_output=True,
         text=True,
         check=True,
